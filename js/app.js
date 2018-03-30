bc728d19c466da38761976129d7297ac1e121519
Tom Petty <hello@anothertompetty.com>
2018-03-30 13:51:30 +0100

    adding new hero and responsive styles

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).on('ready', function() {
+
+  // Fade in nav
+
+  $('#trigger-menu').waypoint(function() {
+    var menu = $('.menu');
+    menu.toggle();
+    menu.toggleClass('bounceInDown');
+  });
+
+  // Smooth scroll
+
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 100
+          }, 400);
+          return false;
+        }
+      }
+    });
+  });
+
+});
